16bf2d65b7328686a3950111901a6f37e312d5cb
Caroline <4971715+carolineBda@users.noreply.github.com>
2026-01-14 16:03:48 +0100

    Delete packages/code-du-travail-frontend/app directory

--- packages/code-du-travail-frontend/app/api/enterprises/route.ts
+++
@@ -1,8 +0,0 @@
-import { EnterprisesAppController } from "../../../src/api/modules/enterprises/controller";
-
-export const dynamic = "force-dynamic";
-
-export async function GET(request: Request) {
-  const controller = new EnterprisesAppController(request);
-  return controller.get();
-}
--- packages/code-du-travail-frontend/app/api/suggest/route.ts
+++
@@ -1,8 +0,0 @@
-import { SuggestAppController } from "../../../src/api/modules/suggest/controller";
-
-export const dynamic = "force-dynamic";
-
-export async function GET(request: Request) {
-  const controller = new SuggestAppController(request);
-  return controller.get();
-}
